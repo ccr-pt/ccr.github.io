b7c3665260824cc7b317ec41c2dc991e8ae3d6f4
Victor <69856209+coding-victor@users.noreply.github.com>
2025-07-28 23:44:58 +0100

    init commit

---
+++ lib/components/toogle_buttom.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:toggle_switch/toggle_switch.dart';
+
+class ToggleButtonWidget extends StatelessWidget {
+  const ToggleButtonWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ToggleSwitch(
+      minWidth: 90.0,
+      cornerRadius: 20.0,
+      activeBgColors: [
+        [Colors.redAccent], // Selected background for option 1
+        [Colors.redAccent]  // Selected background for option 2
+      ],
+      activeFgColor: Colors.white,
+      inactiveBgColor: Colors.grey,     // Unselected background
+      inactiveFgColor: Colors.white,    // Unselected foreground (text)
+      initialLabelIndex: 0,
+      totalSwitches: 2,
+      labels: const ['Militante', 'Comité'],
+      radiusStyle: true,
+      onToggle: (index) {
+        Future.delayed(const Duration(milliseconds: 1000), () {
+    print('switched to: $index');
+    // You can also trigger other delayed logic here
+  });
+},
+
+    );
+  }
+}
